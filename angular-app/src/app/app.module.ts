faaa454ea138b051377fdc7ee294cbb417f16d93
Makasin22 <maxim.kiryakov@gmail.com>
2025-07-12 15:18:16 +0300

    1

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { WikiCardComponent } from './wiki-card/wiki-card.component';
 
-@NgModule({
-  declarations: [AppComponent, WikiCardComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [AppComponent, WikiCardComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule, FormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
