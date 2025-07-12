cf26ac080895184cef2461d79e76d26ec28783cf
Makasin22 <60397552+Makasin22@users.noreply.github.com>
2025-07-12 16:16:02 +0300

    fix app module http client

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { WikiCardComponent } from './wiki-card/wiki-card.component';
 
-@NgModule({ declarations: [AppComponent, WikiCardComponent],
-    bootstrap: [AppComponent], imports: [BrowserModule, FormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+@NgModule({
+  declarations: [AppComponent, WikiCardComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  bootstrap: [AppComponent],
+  providers: []
+})
 export class AppModule {}
